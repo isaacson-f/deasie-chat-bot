24eeae2ad17200d962d4aeb9200cff62fada353f
Frank Isaacson <isaacson.f@northeastern.edu>
2024-10-04 18:57:44 -0400

    backend readme

--- backend/routers/chat_router.py
+++ backend/routers/chat_router.py
@@ -16,17 +16,6 @@ router = APIRouter(
 logger = logging.getLogger(__name__)
 
 
-
-async def get_cookie_or_token(
-    websocket: WebSocket,
-    session: Annotated[str | None, Cookie()] = None,
-    token: Annotated[str | None, Query()] = None,
-):
-    if session is None and token is None:
-        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
-    return session or token
-
-
 @router.websocket("/{user_id}")
 async def websocket(websocket: WebSocket, 
                     chat_service: Annotated[ChatService, Depends(ChatService)], 
@@ -38,6 +27,7 @@ async def websocket(websocket: WebSocket,
             logger.info(f"User {user_id} not found, creating...")
             user = User(_id=user_id)
             cur_user = await user_service.create_user(user)
+        
         # Check if conversation exists, if not create it
         conversations = chat_service.get_conversations_by_user(user_id)
         if len(conversations) == 0:
@@ -48,7 +38,6 @@ async def websocket(websocket: WebSocket,
         await websocket.send_text("######CONVERSATIONS######")
         if len(conversations) > 5:
             for past_convo in conversations[-5:]:
-                logger.info(f"Past conversation: {past_convo.conversation_id}")
                 # send the conversation id
                 await websocket.send_text(past_convo.conversation_id)
                 # send the first message in the conversation
@@ -56,17 +45,16 @@ async def websocket(websocket: WebSocket,
                     await websocket.send_text(past_convo.messages[0].content)
         else:
             for past_convo in conversations:
-                logger.info(f"Past conversation other flow: {past_convo.conversation_id}")
                 # send the conversation id
                 await websocket.send_text(past_convo.conversation_id)
                 # send the first message in the conversation
                 if len(past_convo.messages) > 0:
                     await websocket.send_text(past_convo.messages[0].content)        
             await websocket.send_text("######ALL_CONVERSATIONS######")
-
+        #begin the chat loop
         while True:
-            # Check if user exists, if not create it
             data = await websocket.receive_text()
+            # Switch conversations if the client sends the switch command
             if data == "#####SWITCH_CONVERSATION######":
                 conversation_id = await websocket.receive_text()
                 conversation = chat_service.get_conversation_by_id(conversation_id)
@@ -80,6 +68,7 @@ async def websocket(websocket: WebSocket,
                 await websocket.send_text("######CONVERSATION_SWITCHED######")
                 continue
 
+            # Send the message to the chatbot and get the response
             chats = chat_service.chat(data, conversation_history=conversation.messages)
             full_message = ""
             chunks = []
